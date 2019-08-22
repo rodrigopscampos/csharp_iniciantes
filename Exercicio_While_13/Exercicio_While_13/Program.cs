bee03663729f78377bea64f2a8e36f2ffa9ecf3e
Rodrigo Campos <rodigopscampos@gmail.com>
2019-08-22 19:47:08 -0300

    adiciona ao repositorio

---
+++ Exercicio_While_13/Exercicio_While_13/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_While_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Peça um número menor que 100 e imprima todos até o 100
+
+            //declara a variável fora do laço, para ela existir depois do laço.
+            int n;
+
+            do
+            {
+                //utiliza um do while, para executar o corpo no mínimo uma vez.
+
+                Console.WriteLine("Informe um número MENOR que 100");
+                n = int.Parse(Console.ReadLine());
+            }
+            while (n >= 100); //sai do laço quando o número for menor que 100.
+
+            //se chegou aqui, o número com certeza é menor que cem.
+            while (n <= 100)
+            {
+                //enquanto for menor ou igual a cem, imprime e incrementa.
+                Console.WriteLine(n);
+                n++;
+            }
+
+            //apenar para esperar o usuário pressionar Enter
+            Console.ReadLine();
+        }
+    }
+}
