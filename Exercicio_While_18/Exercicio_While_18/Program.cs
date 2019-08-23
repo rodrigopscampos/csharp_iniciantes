589367edda749c52a774c63efe37d3e80e9307d6
Rodrigo Campos <rodigopscampos@gmail.com>
2019-08-23 20:32:44 -0300

    adiciona correcoes

--- Exercicio_While_18/Exercicio_While_18/Program.cs
+++ Exercicio_While_18/Exercicio_While_18/Program.cs
@@ -10,6 +10,49 @@ namespace Exercicio_While_18
     {
         static void Main(string[] args)
         {
+            //Infinitamente, some os valores digitados e 
+            //imprima o valor, a quantidade de amostras e a média
+
+            //variável para guarda o acumulado (soma de tudo que foi digitado)
+            float acumulado = 0;
+
+            //variável para guardar a quantidade de amostras (quantidade de números passados pelo usuário)
+            int qtdAmostras = 0;
+
+            while (true) //infinitamente
+            {
+                //se a quantidade de amostar é zero, significa que é a primeira vez
+                if (qtdAmostras == 0)
+                {
+                    //imprime sem quebrar de linha
+                    Console.Write("Número: ");
+                }
+                else
+                {
+                    //se cair aqui, siginifica que não é a primeira vez
+                    //nesta caso, imprime uma quebra de linha antes do texto "Número: "
+                    Console.Write("\nNúmero: ");
+                }
+
+                //lê a amostra do usuário, como um float
+                float n = float.Parse(Console.ReadLine());
+
+                //a quantidade de amostras incrementa (soma 1)
+                qtdAmostras++;
+
+                //a variável acumulado, acumula a amostra
+                acumulado += n;
+
+                //calcula a média
+                float media = acumulado / qtdAmostras;
+
+                //imprime o acumulado, qtd amostras e média
+                Console.WriteLine(
+                    "Acumulado: " + acumulado
+                    + " Qtd. Amostras: " + qtdAmostras
+                    + " Média: " + media
+                    );
+            }
         }
     }
 }
