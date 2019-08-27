393b95b2e3a17f3c594b716e7c0005d26c11d942
Rodrigo Campos <rodigopscampos@gmail.com>
2019-08-26 21:51:20 -0300

    adiciona exercicio for

---
+++ Exercicio_For_TrianguloReto/Exercicio_For_TrianguloReto/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_For_TrianguloReto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int altura = 10;
+
+            for (int v = 1; v <= altura; v++)
+            {
+                for (int h = 1; h <= v; h++)
+                {
+                    Console.Write("*");
+                    System.Threading.Thread.Sleep(100);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
