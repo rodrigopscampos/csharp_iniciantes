4f515e567ae53069db591c29d8f1e677de740771
Rodrigo Campos <rodigopscampos@gmail.com>
2019-08-28 20:36:25 -0300

    adiciona ascii arts

---
+++ Exercicio_For_Trapezio/Exercicio_For_Trapezio/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_For_Trapezio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int baseMenor = 6;
+
+            for(int v = 0; v <= baseMenor * 2; v++)
+            {
+                int inicio;
+                int fim;
+
+                if(v <= baseMenor)
+                {
+                    inicio = baseMenor + 1 - v;
+                    fim = (baseMenor * 2) + v;
+                }
+                else
+                {
+                    inicio = 1;
+                    fim = baseMenor * 3;
+                }
+
+                for(int h = 0; h <= baseMenor * 3; h++)
+                {
+                    if(h >= inicio && h <= fim)
+                    {
+                        Console.Write("*");
+                        System.Threading.Thread.Sleep(50);
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercicio_For_TrianguloEquilatero/Exercicio_For_TrianguloEquilatero/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_For_TrianguloEquilatero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int centro = 5;
+
+            for (int v = 0; v <= centro; v++)
+            {
+                int inicio = centro - v;
+                int fim = centro + v;
+
+                for (int h = 0; h <= centro * 2; h++)
+                {
+                    if (h >= inicio && h <= fim)
+                    {
+                        Console.Write("*");
+                        System.Threading.Thread.Sleep(50);
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            //parte de baixo
+            for (int v = 0; v <= centro - 1; v++)
+            {
+                int inicio = v + 1;
+                int fim = (centro * 2) - 1 - v;
+
+                for (int h = 0; h <= (centro * 2) - 1; h++)
+                {
+                    if (h >= inicio && h <= fim)
+                    {
+                        Console.Write("*");
+                        System.Threading.Thread.Sleep(50);
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
