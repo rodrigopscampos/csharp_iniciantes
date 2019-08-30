b090aade98b85a1b745845be4a9cf95ee4e04183
Rodrigo Campos <rodigopscampos@gmail.com>
2019-08-29 21:26:03 -0300

    adiciona exercicios

---
+++ Exercicio_Array_01/Exercicio_Array_01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Array_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crie um array de inteiros de comprimento 10000 
+            //e preencha com os números de 1 a 10000
+
+            int[] array = new int[10000];
+
+            for (int i = 0; i < 10000; i++)
+            {
+                array[i] = i + 1;
+            }
+        }
+    }
+}
---
+++ Exercicio_Array_02/Exercicio_Array_02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Array_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crie um array de tamanho informado 
+            //pelo usuário, preencha e imprima o conteúdo do array
+
+            Console.Write("Informe o tamanho do array: ");
+            int tam = int.Parse(Console.ReadLine());
+
+            string[] array = new string[tam];
+
+            for (int i = 0; i < tam; i++)
+            {
+                Console.Write("Conteúdo " + (i + 1) + ": ");
+                array[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("---------------------");
+            //exercicio 2
+            //for(int i = 0; i < tam; i++)
+            //{
+            //    Console.WriteLine(array[i]);
+            //}
+
+            //exercicio 3
+            for (int i = tam - 1; i >= 0; i--)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
