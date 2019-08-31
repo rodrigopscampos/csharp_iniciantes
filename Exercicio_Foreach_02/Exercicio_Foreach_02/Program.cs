b158844383132db4b230f160493d492b4acd2def
Rodrigo Campos <rodigopscampos@gmail.com>
2019-08-30 21:41:49 -0300

    adiciona exercicio

---
+++ Exercicio_Foreach_02/Exercicio_Foreach_02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Foreach_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Some o valor de todos os ítens de um array de int utilizando foreach
+
+            int[] numeros = new[] { 3, 2, 4, 1, 3, 45, 6, 3, 23, 34, 6 };
+
+            int soma = 0;
+
+            foreach (int item in numeros)
+            {
+                soma = soma + item;
+            }
+
+            Console.WriteLine("Soma = " + soma);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercicio_Foreach_03/Exercicio_Foreach_03/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Foreach_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implemente um algoritmo que imprima um array de maneira agradável
+
+            int[] array = new[] { 1, 2, 4, 6, 2, 1, 3, 100 };
+
+            Console.Write("[ ");
+            foreach (int item in array)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+            Console.Write("]");
+            Console.WriteLine();
+
+            //Percorra um array e imprima todos os 
+            //números pares e depois todos os números ímpares
+            Console.Write("Pares: [ ");
+            foreach(int item in array)
+            {
+                if(item % 2 == 0)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+            Console.Write("]");
+            Console.WriteLine();
+
+            Console.Write("Impares: [ ");
+            foreach(int item in array)
+            {
+                if(item % 2 == 1)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+            Console.Write("]");
+
+            Console.ReadLine();
+        }
+    }
+}
